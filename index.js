ae4156a84c37c9281ea0888a4bf0c43df64ef897
Peter Czibik <p.czibik@gmail.com>
2015-03-01 00:04:00 +0100

    Modularized configuration files and changed the email text.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var server = require('./lib/Server');
 
-var config = require('./lib/config');
+var config = require('./lib/config/server');
 
 server.listen(config.port, function() {
-  console.log('Server is listening at ' + config.port);
+  console.log('Server is listening at port: ' + config.port);
 });
